cde8b7840d0d7952a6241de667ac77de9dd41ab4
Smailey <smailey.behm@gmail.com>
2023-06-26 20:27:56 -0300

    refatoração do tabnav, transformado em classe

--- js/modules/navegacao-tabs.js
+++ js/modules/navegacao-tabs.js
@@ -1,26 +1,37 @@
-export default function iniciarNavTab() {
-  const tabMenu = document.querySelectorAll("[data-tab='menu'] li")
-  const tabContent = document.querySelectorAll("[data-tab='content'] section")
-  if (tabMenu.length && tabContent.length) {
-    //deixando o primeiro item da lista já ativado
-    tabContent[0].classList.add("ativo")
-
-    function activeTab(index) {
-      //remove a classe 'ativo' das sections pra depois incluir em uma específica
-      //removendo
-      tabContent.forEach((section) => {
-        section.classList.remove("ativo")
-      })
-      const direcaoAnimacao = tabContent[index].dataset.anime
-      //adicionando em uma específica  
-      tabContent[index].classList.add("ativo", direcaoAnimacao)
-    }
-    //adicionando eventlistener para cada imagem
-    tabMenu.forEach((item, index) => {
-      item.addEventListener("click", (event) => {
-        activeTab(index)
-      })
-    })
-
+export default class iniciarNavTab {
+  constructor(menu, content) {
+    this.tabMenu = document.querySelectorAll(menu)
+    this.tabContent = document.querySelectorAll(content)
+    this.activeClass = "ativo"
   }
+  // ativa a tab com base no index
+  activeTab(index) {
+    //remove a classe 'ativo' das sections pra depois incluir em uma específica
+    //removendo
+    this.tabContent.forEach((section) => {
+      section.classList.remove(this.activeClass)
+    })
+    const direcaoAnimacao = this.tabContent[index].dataset.anime
+    //adicionando em uma específica  
+    this.tabContent[index].classList.add(this.activeClass, direcaoAnimacao)
+  }
+  // adiciona os eventos nas tabs
+  addTabNavEvent() {
+    //adicionando eventlistener para cada imagem
+    this.tabMenu.forEach((item, index) => {
+      item.addEventListener("click", () => this.activeTab(index))
+    })
+  }
+
+  init() {
+    if (this.tabMenu.length && this.tabContent.length) {
+      // deixando o primeiro item da lista já ativado
+      this.activeTab(0)
+      // inserindo eventos para ativar demais itens
+      this.addTabNavEvent()
+    }
+  }
+
+
+
 }
