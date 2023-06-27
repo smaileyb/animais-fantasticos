f36556346bfab65047761da31366a5b3b2e43481
Smailey <smailey.behm@gmail.com>
2023-06-26 21:48:12 -0300

    refatoração e novas funções na tooltip

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ import ScrollSuave from "./modules/scroll-suave.js"
 import Accordion from "./modules/menu-accordion.js"
 import TabNav from "./modules/navegacao-tabs.js"
 import Modal from "./modules/modal.js"
-import iniciarTooltip from './modules/tooltip.js';
+import Tooltip from './modules/tooltip.js';
 import iniciarDropDownMenu from './modules/dropdown-menu.js';
 import iniciarMenuMobile from "./modules/menu-mobile.js"
 import iniciarHorarioFuncionamento from "./modules/horario-funcionamento.js"
@@ -23,9 +23,9 @@ tabNav.init()
 //MODAL
 const modal = new Modal("[data-modal='abrir']", "[data-modal='fechar']", "[data-modal='container']")
 modal.init()
-
 //TOOLTIP
-iniciarTooltip()
+const tooltip = new Tooltip('[data-tooltip')
+tooltip.init()
 //DROPDOWN MENU
 iniciarDropDownMenu()
 //MENU MOBILE
