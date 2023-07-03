2fe3eadf4a0e412d6fc685acadedc38e86c2742e
Smailey <smailey.behm@gmail.com>
2023-07-03 14:52:10 -0300

    Dropdown Menu refatorado como Classe

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,24 +1,42 @@
 import outsideClick from "./outsideclick.js"
 
-export default function iniciarDropDownMenu() {
-  const dropdownMenus = document.querySelectorAll("[data-dropdown")
-  // dropdownMenus.forEach((menu)=>{
-  //   menu.addEventListener("click", handleClick)
-  // })
-  // ADICIONAR escutador de evento com mais de um fator desencadeante
-  dropdownMenus.forEach((menu) => {
-    ['touchstart', 'click'].forEach(userEvent => {
-      menu.addEventListener(userEvent, handleClick)
-    })
-  })
+export default class DropDownMenu {
+  constructor(dropdownMenus, triggerEvents) {
+    this.dropdownMenus = document.querySelectorAll(dropdownMenus)
+    this.activeClass = "ativo"
+    /* define o touchstart e o click como argumento padrão para os eventos que darão o trigger no dropdown menu, caso o usuário não especifique os próprios eventos em que quer inserir o Listener */
+    if (this.triggerEvents === undefined) {
+      this.triggerEvents = ['touchstart', 'click']
+    } else {
+      this.triggerEvents = triggerEvents
+    }
 
-  function handleClick(event) {
+    //bind
+    this.activeDropDownMenu = this.activeDropDownMenu.bind(this)
+
+  }
+  // ativa o dropdownmenu e adiciona a função que observa o clique fora dele
+  activeDropDownMenu(event) {
     event.preventDefault()
-    this.classList.add("ativo")
-    outsideClick(this, ['touchstart', 'click'], () => {
-      this.classList.remove("ativo")
+    const element = event.currentTarget
+    element.classList.add(this.activeClass)
+    outsideClick(element, this.triggerEvents, () => {
+      element.classList.remove(this.activeClass)
     })
-
+  }
+  // adiciona os eventos para ativar o dropdown menu
+  addListener() {
+    this.dropdownMenus.forEach((menu) => {
+      this.triggerEvents.forEach(userEvent => {
+        menu.addEventListener(userEvent, this.activeDropDownMenu)
+      })
+    })
+  }
+  // inicia os métodos da classe
+  init() {
+    if (this.dropdownMenus.length) {
+      this.addListener()
+    }
   }
 }
 
